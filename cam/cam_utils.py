e1867e0c5bb1a6773a44bfd98e2a4f99358b1971
gerlaxrex <gerlaxrex@gmail.com>
2022-06-15 20:28:42 +0200

    Small fixes to GradCAM functions

--- cam/cam_utils.py
+++ cam/cam_utils.py
@@ -1,8 +1,13 @@
 import tensorflow as tf
 import matplotlib.pyplot as plt
-from configs import IMG_SHAPE
+from configs import IMG_SHAPE, CLASS_LIST
 import numpy as np
 import matplotlib.cm as cm
+from PIL import Image
+import logging
+
+logger = logging.getLogger(__file__)
+
 
 def make_gradcam_heatmap(img, model):
     """Generate the heatmap for using the GradCAM algorithm"""
@@ -19,7 +24,8 @@ def make_gradcam_heatmap(img, model):
     heatmap = tf.squeeze(heatmap)
 
     heatmap = tf.maximum(heatmap, 0) / tf.math.reduce_max(heatmap)
-    return heatmap.numpy()
+    return heatmap.numpy(), preds[0]
+
 
 def transform_heatmap(heatmap):
     """Transform the heatmap obtained from the gradients into a JET image for visualization purposes"""
@@ -38,11 +44,37 @@ def transform_heatmap(heatmap):
     jet_heatmap = jet_heatmap.resize(IMG_SHAPE)
     return jet_heatmap
 
-def visualize_result(img, heatmap):
-    fig, ax = plt.subplots(1,3,figsize=(20,10))
-    jet_heatmap = transform_heatmap(heatmap)
+
+def visualize_result(img, heatmap, preds, true_class):
+    fig, ax = plt.subplots(1, 3, figsize=(10, 5))
+    predictions = {v: tf.nn.softmax(preds).numpy()[k] for k, v in enumerate(CLASS_LIST)}
+    fig.supxlabel(f'Predictions: {predictions}', fontsize=18)
+    fig.suptitle(f'True label: {true_class}', fontsize=14)
     ax[0].imshow(img)
-    ax[1].imshow(jet_heatmap)
+    ax[0].set_title('Original Image')
+    ax[1].imshow(heatmap)
+    ax[1].set_title('Heatmap')
+    ax[2].set_title('Overlay (Heatmap + Image)')
     ax[2].imshow(img)
-    ax[2].imshow(jet_heatmap, alpha = 0.3)
-    plt.imshow()
+    ax[2].imshow(heatmap, alpha=0.3)
+    plt.show()
+
+
+def preprocess_img(path):
+    img = tf.convert_to_tensor(Image.open(path).resize(IMG_SHAPE))
+    img = tf.expand_dims(img, axis=0)
+    return img
+
+
+def perform_cam(path, model, true_class):
+    img = preprocess_img(path)
+    heatmap, preds = make_gradcam_heatmap(img, model)
+    heatmap = transform_heatmap(heatmap)
+
+    logger.info(f'Predictions: {preds}')
+    logger.info(f'True class: {true_class}')
+
+    # Squeeze the first dimension of the image
+    img = tf.squeeze(img)
+    visualize_result(img, heatmap, preds, true_class)
+    return heatmap, preds
